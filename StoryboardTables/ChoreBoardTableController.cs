809e016e8a8ad74c775e29413cd8067453657e1f
Brian Bird <birdb@lanecc.edu>
2020-02-26 15:15:54 -0800

    Finsihed the tutorial. Everything works!

--- StoryboardTables/ChoreBoardTableController.cs
+++ StoryboardTables/ChoreBoardTableController.cs
@@ -23,5 +23,55 @@ namespace StoryboardTables
 
             TableView.Source = new RootTableSource(chores.ToArray());
         }
+
+        public override void ViewDidLoad()
+        {
+            AddButton.Clicked += (sender, e) => CreateTask();
+        }
+    
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            if (segue.Identifier == "TaskSegue")
+            { // set in Storyboard
+                var detailController = segue.DestinationViewController as TaskDetailViewController;
+                if (detailController != null)
+                {
+                    var source = TableView.Source as RootTableSource;
+                    var rowPath = TableView.IndexPathForSelectedRow;
+                    var item = source.GetItem(rowPath.Row);
+                    detailController.SetTask(this, item); // to be defined on the TaskDetailViewController
+                }
+
+            }
+        }
+
+        public void SaveTask(Chore chore)
+        {
+            var oldTask = chores.Find(t => t.Id == chore.Id);
+            NavigationController.PopViewController(true);
+        }
+
+        public void DeleteTask(Chore chore)
+        {
+            var oldTask = chores.Find(t => t.Id == chore.Id);
+            chores.Remove(oldTask);
+            NavigationController.PopViewController(true);
+        }
+
+        public void CreateTask()
+        {
+            // first, add the task to the underlying data
+            var newId = chores[chores.Count - 1].Id + 1;
+            var newChore = new Chore { Id = newId };
+            chores.Add(newChore);
+
+            // then open the detail view to edit it
+            var detail = Storyboard.InstantiateViewController("detail") as TaskDetailViewController;
+            detail.SetTask(this, newChore);
+            NavigationController.PushViewController(detail, true);
+        }
+
+        public override 
     }
 }
--- StoryboardTables/TaskDetailViewController.cs
+++ StoryboardTables/TaskDetailViewController.cs
@@ -9,5 +9,35 @@ namespace StoryboardTables
         public TaskDetailViewController (IntPtr handle) : base (handle)
         {
         }
+
+        Chore CurrentTask { get; set; }
+        public ChoreBoardTableController Delegate { get; set; } // will be used to Save, Delete later
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            TitleText.Text = CurrentTask.Name;
+            NotesText.Text = CurrentTask.Notes;
+            DoneSwitch.On = CurrentTask.Done;
+        }
+
+        // this will be called before the view is displayed
+        public void SetTask(ChoreBoardTableController d, Chore task)
+        {
+            Delegate = d;
+            CurrentTask = task;
+        }
+
+        public override void ViewDidLoad()
+        {
+            SaveButton.TouchUpInside += (sender, e) => {
+                CurrentTask.Name = TitleText.Text;
+                CurrentTask.Notes = NotesText.Text;
+                CurrentTask.Done = DoneSwitch.On;
+                Delegate.SaveTask(CurrentTask);
+            };
+
+            DeleteButton.TouchUpInside += (sender, e) => Delegate.DeleteTask(CurrentTask);
+        }
     }
 }
